e00139224b0cdc05ff96456a86bd8971c008aa65
ibraheemmmoosa <45825930+ibraheemmmoosa@users.noreply.github.com>
2021-02-26 19:21:57 +0600

    Print list of authors from ebanglalibrary

--- scrapy-projects/ebanglalibrary/ebanglalibrary/spiders/lekhok.py
+++ scrapy-projects/ebanglalibrary/ebanglalibrary/spiders/lekhok.py
@@ -3,14 +3,11 @@ import scrapy
 
 class LekhokSpider(scrapy.Spider):
     name = "lekhok"
-
-    def start_requests(self):
-        urls = ['https://www.ebanglalibrary.com/%E0%A6%B2%E0%A7%87%E0%A6%96%E0%A6%95-%E0%A6%B0%E0%A6%9A%E0%A6%A8%E0%A6%BE/']
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = ['https://www.ebanglalibrary.com/%E0%A6%B2%E0%A7%87%E0%A6%96%E0%A6%95-%E0%A6%B0%E0%A6%9A%E0%A6%A8%E0%A6%BE/']
 
     def parse(self, response):
-        print(response)
-        print(response.url)
-        print(response.body)
+        authors = []
+        for author in response.xpath('//div[@id="writers-list"]/code/ul/li/a/text()').getall():
+            authors.append(author)
+            print(author)
         self.log('Got response!')
